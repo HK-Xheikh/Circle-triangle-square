5561fe4de2f3b313f692153ecd16ec8918e58b31
HK-Xheikh <hariskhan1151@gmail.com>
2024-08-18 02:35:24 +0500

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,16 @@
+# triangle#
+height=59
+base=6
+f=0.5
+area=f*base*height
+print("area of triangle",area)
+#circle#
+radius=8
+pie=3-14
+area=radius*pie
+print("area of circle",area)
+# square#
+side=5
+side=5
+area=side*side
+print("area of square",area)
